fdb577df7c1504757fb5a74637d4e162e2ea80ff
Rodrigo Candido <>
2021-08-07 18:42:17 +0200

    On Alert --> Add alert button inside AlertView

--- components/lib/src/views/Alert/alert_view.dart
+++ components/lib/src/views/Alert/alert_view.dart
@@ -10,6 +10,57 @@ class AlertView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert View'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Show Alert'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: StadiumBorder()
+          ),
+          onPressed: (){
+            _showAlert(context);
+          },
+        ),
+      ),
     );
   }
+
+
+  _showAlert(BuildContext context){
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+            return AlertDialog(
+              title: Text('Aler Title'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Content of Alert Dialog'),
+                  FlutterLogo(size: 100)
+                ],
+              ),
+              actions: [
+                TextButton(
+                    child: Text('Cancelar'),
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    },
+                ),
+                
+                TextButton(
+                  child: Text('Ok'),
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                    },
+                )
+              ],
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            );
+        },
+    );
+  }
+
+
+
 }
